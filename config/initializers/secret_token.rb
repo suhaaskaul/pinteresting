55c2f5be0c8bb9bda71b5a8e8c2e35312af7a048
Suhaas kaul <suhaas.kaul@outlook.com>
2014-04-04 19:05:29 +0530

    Ruby on Rails App : Pinterest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = '5d1e5952a1a52a41e1cffb3de64dea254c39aca28f9a1914bc16cc7dbfe17bdf2596d1f3c69b5869e683bff411322cd3519e93bb25b62583a4b6f56787fec9d7'
